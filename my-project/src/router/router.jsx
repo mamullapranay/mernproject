e2858480181a773b4071224dbd5bf2b998c435a2
pranay360 <mamullapranay360@gmail.com>
2024-05-21 10:03:47 +0530

    commit

--- my-project/src/router/router.jsx
+++ my-project/src/router/router.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
         element: <Main/>,
         children:[{
             path: "/",
-            element:<Home/>,}
+            element:<Home/>,},
         ]
     },
 ]);
